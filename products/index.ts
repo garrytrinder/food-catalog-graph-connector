e4deb78d61e8f5c5ea10dd1f5f837287091c1a1a
Garry Trinder <garry@trinder365.co.uk>
2023-10-20 17:04:06 +0100

    Update products HTTP Trigger

--- products/index.ts
+++ products/index.ts
@@ -1,10 +1,11 @@
 import { TableClient } from "@azure/data-tables";
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { randomUUID } from "crypto";
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     const tableClient = TableClient.fromConnectionString(process.env.AzureWebJobsStorage, 'products');
 
-    const { method } = req;
+    const { method, body } = req;
     const { id } = req.query;
 
     // GET api/products
@@ -23,34 +24,66 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
 
     // GET api/products?id={id}
     if (method === 'GET' && id) {
-        const product = await tableClient.getEntity('products', id);
-        context.res = {
-            status: 200,
-            body: product
+        try {
+            const product = await tableClient.getEntity('products', id);
+            context.res = {
+                status: 200,
+                body: product
+            }
+        } catch (error) {
+            context.res = {
+                status: error.statusCode,
+            }
         }
     }
 
     // POST api/products
-    if (method === 'POST') {
-        context.res = {
-            status: 501,
-            body: 'Not implemented'
+    if (method === 'POST' && !id) {
+        try {
+            const newProduct = {
+                partitionKey: "products",
+                rowKey: randomUUID().replace(/-|[a-z]/g, ''),
+                last_modified_t: Date.now(),
+                ...body,
+            }
+            await tableClient.createEntity(newProduct);
+            context.res = {
+                status: 201
+            }
+        } catch (error) {
+            context.res = {
+                status: error.statusCode,
+            }
         }
     }
 
     // PATCH api/products?id={id}
     if (method === 'PATCH' && id) {
-        context.res = {
-            status: 501,
-            body: 'Not implemented'
+        try {
+            const product = await tableClient.getEntity("products", id);
+            await tableClient.updateEntity({ ...product, ...body, last_modified_t: Date.now(), }, "Merge");
+            context.res = {
+                status: 200
+            }
+        } catch (error) {
+            context.res = {
+                status: error.statusCode,
+            }
         }
     }
 
     // DELETE api/products?id={id}
     if (method === 'DELETE' && id) {
-        context.res = {
-            status: 501,
-            body: 'Not implemented'
+        try {
+            await tableClient.getEntity("products", id);
+            await tableClient.deleteEntity('products', id);
+            context.res = {
+                status: 200
+            }
+        } catch (error) {
+            context.res = {
+                status: error.statusCode,
+            }
         }
     }
 };
